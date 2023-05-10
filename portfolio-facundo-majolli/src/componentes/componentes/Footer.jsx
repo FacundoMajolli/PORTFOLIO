15218fe07d1bca72ab5586482abc5cc9ab5f7e8b
Facundo Majolli <facundomajolli12@gmail.com>
2023-05-10 18:01:17 -0300

    Cargando componentes

---
+++ portfolio-facundo-majolli/src/componentes/componentes/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function Footer() {
+  
+  return (
+    <footer>
+      <h1>
+      footer
+
+      </h1>
+      </footer>
+  );
+
+  }
+export default Footer;
+
---
+++ portfolio-facundo-majolli/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from '../componentes/componentes/Header';
+import Main from '../componentes/componentes/Main';
+import Footer from '../componentes/componentes/Footer';
+
+export const Home = () => {
+    
+  return (
+    <div>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div> 
+  );
+};
+
+export default Home;
