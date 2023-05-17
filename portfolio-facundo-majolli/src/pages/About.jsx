e2f8ee76a135c590dec69daf04a183a9020483e8
Facundo Majolli <facundomajolli12@gmail.com>
2023-05-16 22:57:01 -0300

    Agregue pages about y contact, agregue  componentes MainContact y MainAbout

---
+++ portfolio-facundo-majolli/src/pages/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../componentes/componentes/Header'
+import MainAbout from '../componentes/componentes/MainAbout'
+import Footer from '../componentes/componentes/Footer'
+
+const About = () => {
+  return (
+    <div>
+        <Header/>
+        <MainAbout/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default About
---
+++ portfolio-facundo-majolli/src/pages/Contact.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import MainContact from '../componentes/componentes/MainContact';
+import Footer from '../componentes/componentes/Footer';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./Home";
+
+
+
+
+export const Contact = () => {
+    
+  
+
+
+  return (
+    <div>
+      <Home/>
+      <MainContact/>
+      <Footer/>
+    </div> 
+  );
+};
+
+export default Contact;
